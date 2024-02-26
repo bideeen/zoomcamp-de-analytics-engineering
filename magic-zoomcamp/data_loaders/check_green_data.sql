418b5715ba1af26fe332f28e63b2a054e6345acf
bideeen <b.abideen@yahoo.com>
2024-02-26 17:14:37 +0100

    mage-all

---
+++ magic-zoomcamp/data_loaders/check_green_data.sql
@@ -0,0 +1,4 @@
+-- Docs: https://docs.mage.ai/guides/sql-blocks
+-- select * from magic.green_trip_data limit 10;
+-- select * from magic.yellow_trip_data limit 10;
+select * from magic.fhv_trip_data limit 10;
---
+++ magic-zoomcamp/data_loaders/load_green_data.py
@@ -0,0 +1,61 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    # urls = [
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-01.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-02.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-03.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-04.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-05.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-06.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-07.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-08.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-09.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-10.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-11.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-12.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-01.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-02.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-03.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-04.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-05.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-06.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-07.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-08.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-09.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-10.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-11.parquet',
+    #     'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-12.parquet',
+    #     ]
+ 
+    # # all data
+    # dfs = []
+
+    # # get all green data
+    # for url in urls:
+    #     df = pd.read_parquet(url, engine='auto')
+    #     dfs.append(df)
+
+    # return pd.concat(dfs, ignore_index = True)
+    url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-04.parquet'
+
+    return pd.read_parquet(url, engine='auto')
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
